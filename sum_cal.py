14d7f14ca7e37b7206d531f4826cbb00e54833b7
hashan1998 <62441276+hashan1998@users.noreply.github.com>
2021-10-08 23:51:19 +0530

    Add files via upload

---
+++ sum_cal.py
@@ -0,0 +1,16 @@
+i=True
+sum=0
+count = 0
+while i== True:
+    num =int(input("enter number"))
+
+    if num !=0:
+        sum=sum+num
+        count=count+1
+
+    else:
+        i=False
+print(sum)
+print(sum/count)
+
+        
